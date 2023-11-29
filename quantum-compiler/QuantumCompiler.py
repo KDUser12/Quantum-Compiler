407e58372b878ec3d04fb4398d2eed1ae2b4ec29
KDUser <128939316+KDUser12@users.noreply.github.com>
2023-11-29 22:19:37 +0100

    Utils : Version

--- quantum-compiler/QuantumCompiler.py
+++ quantum-compiler/QuantumCompiler.py
@@ -3,5 +3,8 @@
 # This program allows you to create executable files but also compile files for better security when publishing your
 # program. It also allows you to create setups for your own program.
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+from apps.utils.version import get_version
+
+VERSION = (1, 0, 0, 'release', 0)
+
+__version__ = get_version(VERSION)
