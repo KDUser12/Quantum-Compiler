89e36d1bb903f2e6971ce75b2b97d346518c23ac
KDUser <128939316+KDUser12@users.noreply.github.com>
2023-11-29 18:40:21 +0100

    Start project

---
+++ quantum-compiler/QuantumCompiler.py
@@ -0,0 +1,7 @@
+# Quantum Compiler is a free open-source compilation program.
+
+# This program allows you to create executable files but also compile files for better security when publishing your
+# program. It also allows you to create setups for your own program.
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
